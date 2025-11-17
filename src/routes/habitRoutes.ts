c28752ef387bb95fadabfbf396c3573767231ba0
Moat <schluesselfehlt@gmail.com>
2025-11-17 12:04:13 +0100

    fixed syntax errors

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -2,30 +2,30 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.get('/'), (req, res) => {
-	req.json({message: 'all habits'})
-}
+router.get('/', (req, res) => {
+  res.json({ message: 'all habits' })
+})
 
-router.get('/:id'), (req, res) => {
-	req.json({message: 'one habit'})
-}
+router.get('/:id', (req, res) => {
+  res.json({ message: 'one habit' })
+})
 
-router.post('/'), (req, res) => {
-	req.json({message: 'created habit'}).status(201)
-}
+router.post('/', (req, res) => {
+  res.json({ message: 'created habit' }).status(201)
+})
 
 
-router.delete('/:id'), (req, res) => {
-	req.json({message: 'deleted habit'})
-}
+router.delete('/:id', (req, res) => {
+  res.json({ message: 'deleted habit' })
+})
 
 // make up new routes
-router.post('/:id/complete'), (req, res) => {
-	req.json({message: 'completed habit'})
-}
+router.post('/:id/complete', (req, res) => {
+  res.json({ message: 'completed habit' })
+})
 
-routerget('/:id/stats', (req, res) => {
-	res.json({message: `Get stats for habit ${req.params.id}`})
+router.get('/:id/stats', (req, res) => {
+  res.json({ message: `Get stats for habit ${req.params.id}` })
 })
 
 export default router
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,19 +3,19 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-	res.json({message: 'users'})
+  res.json({ message: 'users' })
 })
 
 router.get('/:id', (req, res) => {
-	res.json({message: 'got users'})
+  res.json({ message: 'got users' })
 })
 
 router.put('/:id', (req, res) => {
-	res.json({message 'user updated'})
+  res.json({ message: 'user updated' })
 })
 
 router.delete('/:id', (req, res) => {
-	res.json({message `Delete user ${req.params.id}`})
+  res.json({ message: `Delete user ${req.params.id}` })
 })
 
 export default router
